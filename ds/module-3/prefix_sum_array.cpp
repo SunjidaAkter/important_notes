a8acc27b47fb2445e0fe0f781517ad120b51c382
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-06-08 18:56:09 +0600

    prefix sum concept cleared

---
+++ ds/module-3/prefix_sum_array.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long int n,q;cin>>n>>q;
+    vector<long long int>v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    long long int pre[n];
+    pre[0]=v[0];
+    for(int i=1;i<v.size();i++){
+        pre[i]=v[i]+pre[i-1];
+    }
+    while(q--){
+        long long int l,r;cin>>l>>r;
+        l--;r--;
+        long long int sum=0;
+        if(l==0)sum=pre[r];
+        else sum=pre[r]-pre[l];
+        cout<<sum<<endl;
+    }
+    return 0;
+}
