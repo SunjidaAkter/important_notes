275396ef325e315b80074fb9717b62f10cdbae37
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-07 23:12:45 +0600

    completed

---
+++ algo/module_6/traversal_adjacency_list.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N=1e5+5;
+vector<int> adj[N];
+bool visited[N];
+void dfs(int u){
+    //* section 1
+    //* actions just after entering a node
+    visited[u]=true;
+    cout<<"visiting node: "<<u<<endl;
+    for(int v:adj[u]){
+        //* section 2
+        //* actions before entering a new child/neighbour
+        if(visited[v])continue;
+        dfs(v);
+        //* section 3
+        //* actions after exiting a child/neighbour
+    }
+    //* section 4
+    //* actions before exiting node u
+}
+int main(){
+    int m,n;cin>>n>>m;
+    for(int i=0;i<m;i++){
+        int u,v;cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);//*for undirected graph
+    }
+    // for(int i=1;i<=n;i++){
+    //     cout<<"List "<<i<<": ";
+    //     for(auto j:adj[i]){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    dfs(1);
+    return 0;
+}
