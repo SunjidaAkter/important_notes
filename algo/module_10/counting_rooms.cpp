63e5c83204ef5c71fe42596b882240831369c24f
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-15 10:30:51 +0600

    completed

---
+++ algo/module_10/counting_rooms.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N=1e5+10;
+vector<string>g;
+
+int main(){
+
+    return 0;
+}
+/*
+//*input
+5 8
+########
+#..#...#
+####.#.#
+#..#...#
+########
+//*output
+3
+*/
