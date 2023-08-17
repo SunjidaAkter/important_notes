bc1fbedc07bfe6748b9c6637e839afad4f41be61
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-17 06:29:12 +0600

    mission accomplished 🔥🚀🔥

--- algo/module_11/bellman_ford_algo.cpp
+++ algo/module_11/bellman_ford_algo.cpp
@@ -14,22 +14,36 @@ void bellman_ford(int s){
     d[s]=0;
     for(int i=1;i<n;i++){
         //edge relaxation
-        for(int u=1;u<=n;u++){
-            for(pii vpair:g[u]){
-                int v=vpair.first;
-                int w=vpair.second;
-                if(d[u]!=INF&&d[v]>d[u]+w){
-                    d[v]=d[u]+w;
-                }
+        // for(int u=1;u<=n;u++){
+        //     for(pii vpair:g[u]){
+        //         int v=vpair.first;
+        //         int w=vpair.second;
+        //         if(d[u]!=INF&&d[v]>d[u]+w){
+        //             d[v]=d[u]+w;
+        //         }
+        //     }
+        // }
+        for(auto edge:list_of_edges){
+            int u=edge.first.first;
+            int v=edge.first.second;
+            int w=edge.second;
+            if(d[u]!=INF&&d[v]>d[u]+w){
+                d[v]=d[u]+w;
             }
+
         }
     }
 }
+//*O(|E| |V|)
+//*O(mn) for normal graph or
+//*O(n²) for sparse graph 
+//*O(n³) for dense graph 
 int main(){
     cin>>n>>m;
     while(m--){
         int u,v,w;cin>>u>>v>>w;
-        g[u].push_back({v,w});
+        // g[u].push_back({v,w});
+        list_of_edges.push_back({{u,v},w});
     }
     bellman_ford(1);
     for(int i=1;i<=n;i++){
