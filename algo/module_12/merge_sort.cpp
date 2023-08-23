9ebc5b5c862730828131b9721a62ce07f5232ba8
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-23 18:14:16 +0600

    mission accomplished 🔥🚀🔥

---
+++ algo/module_12/merge_sort.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+void merge(int a[],int l,int m,int r){
+    int leftSize=m-l+1;
+    int rightSize=r-m;
+    int L[leftSize],R[rightSize];
+    int k=0;
+    for(int i=l;i<=m;i++){
+        L[k]=a[i];
+        k++;
+    }
+    k=0;
+    for(int i=m+1;i<=r;i++){
+        R[k]=a[i];
+        k++;
+    }
+    int i=0,j=0,curr=0;
+    while(i<leftSize&& j<rightSize){
+        if(L[i]<=R[j]){
+            a[curr]=L[i];
+            i++;
+        }else{
+            a[curr]=R[j];
+            j++;
+        }
+        curr++;
+    }
+    while(i<leftSize){
+        a[curr]=L[i];
+        i++;
+        curr++;
+    }
+    while(i<leftSize){
+        a[curr]=R[i];
+        i++;
+        curr++;
+    }
+}
+int main(){
+    int n;cin>>n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>a[i];
+    }
+    merge(a,0,3,n-1);
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
