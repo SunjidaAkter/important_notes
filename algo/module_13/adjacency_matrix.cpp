3dab435f478f8f13e8edcf4eab0eab4f064fd114
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-24 14:40:49 +0600

    mission accomplished 🔥🚀🔥

---
+++ algo/module_13/adjacency_matrix.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,e;
+    cin>>n>>e;
+    int adj[n+1][n+1];
+    for(int i=1;i<=n+1;i++){
+        for(int j=1;j<=n+1;j++){
+            adj[i][j]=0;
+            if(i==j)adj[i][j]=1;
+        }
+    }
+    while(e--){
+        int a,b;cin>>a>>b;
+        adj[a][b]=1;
+        adj[b][a]=1;
+    }
+    for(int i=1;i<=n+1;i++){
+        for(int j=1;j<=n+1;j++){
+            cout<<adj[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
