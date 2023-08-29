a2c8fac2114401fa747a5523f4d35e5a428e435e
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-29 17:33:30 +0600

    mission accomplished 🔥🚀🔥

---
+++ algo/module_18/find_disjoint.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int parent[8]={-1,-1,1,1,6,4,-1,-1};
+int find(int n){
+    while(parent[n]!=-1){
+        n=parent[n];
+    }
+    return n;
+}
+int main(){
+    cout<<find(5);
+    return 0;
+}
