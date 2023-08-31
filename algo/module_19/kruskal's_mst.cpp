e154428ccb16fda88e448dab89f6567752a6ca3a
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-31 18:35:58 +0600

    mission accomplished 🔥🚀🔥

--- algo/module_19/kruskal's_mst.cpp
+++ algo/module_19/kruskal's_mst.cpp
@@ -12,15 +12,58 @@ class Edge{
 
 };
 bool cmp(Edge a,Edge b){
-    return a.w>b.w;
+    return a.w<b.w;
 } 
+const int N=1e5+5;
+int parent[N];
+int parentSize[N];
+void dsu_set(int n){
+    for(int i=1;i<=n;i++){
+        parent[i]=-1;
+        parentSize[i]=1;
+    }
+}
+int dsu_find(int n){
+    while(parent[n]!=-1){
+        n=parent[n];
+    }
+    return n;
+}
+void dsu_union(int a,int b){
+    int leaderA=dsu_find(a);
+    int leaderB=dsu_find(b);
+    if(leaderA!=leaderB){
+        if(parentSize[leaderA]>parentSize[leaderB]){
+            parent[leaderB]=leaderA;
+            parentSize[leaderA]+=parentSize[leaderB];
+        }else{
+            parent[leaderA]=leaderB;
+            parentSize[leaderB]+=parentSize[leaderA];
+        }
+    }
+}
 int main(){
     int n,e;cin>>n>>e;
     vector<Edge>v;
+    vector<Edge>ans;
+    dsu_set(n);
     while(e--){
         int a,b,w;cin>>a>>b>>w;
         v.push_back(Edge(a,b,w));
     } 
     sort(v.begin(),v.end(),cmp);
+    for(Edge val:v){
+        int a=val.a;
+        int b=val.b;
+        int w=val.w;
+        int leaderA=dsu_find(a);
+        int leaderB=dsu_find(b);
+        if(leaderA==leaderB)continue;
+        ans.push_back(val);
+        dsu_union(a,b);
+    }
+    for(Edge val:ans){
+        cout<<val.a<<" "<<val.b<<" "<<val.w<<endl;
+    }
     return 0;
 }
