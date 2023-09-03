77027b335c0ffffd9a252edfcb7880f1f3543973
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-09-03 20:28:07 +0600

    mission accomplished 🔥🚀🔥

---
+++ algo/module_21/fibo_optimized_recur.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll N=1e5+5;
+ll save[N];
+ll fibo(int n){
+    if(n==0 ||n==1)return 1;
+    if(save[n]!=-1)return save[n];
+    ll ans1=fibo(n-1);
+    ll ans2=fibo(n-2);
+    save[n]=ans1+ans2;
+    return save[n];
+}
+int main(){
+    int n;cin>>n;
+    for(int i=0;i<N;i++)save[i]=-1;
+    cout<<fibo(n);
+    return 0;
+}
+//*time complexity 2^n
