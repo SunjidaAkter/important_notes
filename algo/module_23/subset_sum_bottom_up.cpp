b741fac8fbd2aab592e6cde0fcdd9e85a5af2de9
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-09-06 16:12:00 +0600

    mission accomplished 🔥🚀🔥

---
+++ algo/module_23/subset_sum_bottom_up.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int,int>pii;
+#define ll long long
+bool dp[1005][1005];
+// bool subset_sum(int n,int a[],int s){
+//     if(n==0){
+//         if(s==0)return true;
+//         else return false;
+//     }
+//     if(a[n-1]<=s){
+//         bool op1=subset_sum(n-1,a,s-a[n-1]);
+//         bool op2=subset_sum(n-1,a,s);
+//         return op1||op2;
+//     }else{
+//         return subset_sum(n-1,a,s);
+//     }
+// }
+int main(){
+    int n;cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)cin>>a[i];
+    int s;cin>>s;
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=s;j++){
+
+        }
+    }
+    // if(subset_sum(n,a,s))cout<<"True";
+    // else cout<<"False";
+    return 0;
+}
