c4d876814d6eabcce8ebd3cecb486810ce4e91a6
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-09-13 20:17:48 +0600

    mission accomplished 🔥🚀🔥

--- algo/module_27/print_shortest_common_supersequence.cpp
+++ algo/module_27/print_shortest_common_supersequence.cpp
@@ -19,6 +19,31 @@ int main(){
            else dp[i][j]=max(dp[i-1][j],dp[i][j-1]); 
         }
     }
-    cout<<n+m-dp[n][m];
+    string ans;
+    int i=n,j=m;
+    while(i!=0&&j!=0){
+        if(a[i-1]==b[j-1]){
+            ans+=a[i-1];
+            i--;j--;
+        }else{
+            if(dp[i-1][j]>dp[i][j-1]){
+                ans+=a[i-1];
+                i--;
+            }else{
+                ans+=b[j-1];
+                j--;
+            }
+        }
+    }
+    while(i!=0){
+        ans+=a[i-1];
+        i--;
+    }
+    while(j!=0){
+        ans+=b[j-1];
+        j--;
+    }
+    reverse(ans.begin(),ans.end());
+    cout<<ans;
     return 0;
 }
