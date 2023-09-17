53ab07c07d9ade9a5513320ac61282354e6ad406
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-09-17 08:01:24 +0600

    mission accomplished 🔥🚀🔥

--- ds/module-3/prefix_sum_array.cpp
+++ ds/module-3/prefix_sum_array.cpp
@@ -4,12 +4,12 @@ int main(){
     long long int n,q;cin>>n>>q;
     vector<long long int>v(n);
     for(int i=0;i<n;i++){
-        cin>>v[i];
+        cin>>v[i];//* 1 2 3 4 5
     }
     long long int pre[n];
-    pre[0]=v[0];
+    pre[0]=v[0];//*1
     for(int i=1;i<v.size();i++){
-        pre[i]=v[i]+pre[i-1];
+        pre[i]=v[i]+pre[i-1];//* 1 3 6 10 15 
     }
     while(q--){
         long long int l,r;cin>>l>>r;
