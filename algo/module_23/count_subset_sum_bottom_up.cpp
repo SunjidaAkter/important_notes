fbefb27ad7b75b3e208bf872732d1757759093a6
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-09-21 14:38:35 +0600

    mission accomplished 🔥🚀🔥

--- algo/module_23/count_subset_sum_bottom_up.cpp
+++ algo/module_23/count_subset_sum_bottom_up.cpp
@@ -21,12 +21,12 @@ int main(){
             }
         }
     }
-    // for(int i=0;i<=n;i++){
-    //     for(int j=0;j<=s;j++){
-    //         cout<<dp[i][j]<<" ";
-    //     }
-    //     cout<<endl;
-    // }
-    cout<<dp[n][s];
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=s;j++){
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    // cout<<dp[n][s];
     return 0;
 }
