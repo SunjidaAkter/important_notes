7ae82462ae0b86d045024be2b92f3bb2fffbb9e9
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-09-24 15:53:53 +0600

    mission accomplished 🔥🚀🔥

--- ds/module-2/vector_find.cpp
+++ ds/module-2/vector_find.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     vector<int>v={1,2,3,2,4,3,4,5,3,9};
-    // vector<int>::iterator it;
+    // *vector<int>::iterator it;
     auto it=find(v.begin(),v.end(),9);
     if(it==v.end())cout<<"Not found";
     cout<<"Found";
