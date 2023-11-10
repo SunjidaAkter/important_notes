79d967d77abe08f00891fc25113502292f8423e2
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-11-10 10:16:39 +0600

    mission accomplished 🔥🚀🔥

---
+++ Tricks/is_prime.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define pub push_back
+#define pob pop_back
+#define nl  '\n'
+#define c(x) cout<<x<<nl
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+typedef pair<int,int>pii;
+#define forl(ty,var,str,end) for(ty var=str; var<end; var++)
+# define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+bool prime(int a)
+{
+    if (a == 1 || a == 0)
+        return false;
+    for (int i=2; i*i <= a; i++)
+        if (a % i == 0)
+            return false;
+    return true;
+}
+void solve(){
+    int n=1;
+    if(prime(n))yes;
+    else no;   
+}
+int main(){
+    FAST;
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
