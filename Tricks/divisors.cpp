e33af4d8e12ca3a69536323a12f425891373d08b
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-11-14 18:56:28 +0600

    mission accomplished 🔥🚀🔥

---
+++ Tricks/divisors.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+typedef pair<ll,ll>pii;
+#define forl(var,str,end) for(long long int var=str; var<end; var++)
+vector<int> getdiv(int n){
+    vector<int> ans;
+    for (int i = 1; i * i <= n; i++){
+        if (n % i == 0){
+            ans.push_back(i);
+            if (n / i != i){
+                ans.push_back(n / i);
+            }
+        }
+    }
+    return ans;
+}
+int main(){
+    int n=100;
+    vector<int> ans=getdiv(n);
+    sort(ans.begin(),ans.end());
+    for(int val:ans)cout<<val<<" ";
+    return 0;
+}
