ba24757165490e6fbea0e051ec046b6baeeb91c2
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-11-27 10:00:37 +0600

    mission accomplished 🔥🚀🔥

---
+++ Tricks/digit_number_of_digits_of_binary_representation.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+typedef pair<ll,ll>pii;
+#define forl(var,str,end) for(long long int var=str; var<end; var++)
+int digits_number(int n){
+    int sz=0;
+    while(n>0){
+        sz++;
+        n/=2;
+    }
+    return sz;
+}
+int main(){
+    int n=2;
+    cout<<digits_number(n);
+    return 0;
+}
