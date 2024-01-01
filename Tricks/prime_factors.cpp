357fee5787076e3b285dbc59e1678412f1d8cda7
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-01-01 19:45:18 +0600

    mission accomplished 🔥🚀🔥

--- Tricks/prime_factors.cpp
+++ Tricks/prime_factors.cpp
@@ -7,9 +7,11 @@ int main(){
     int n=28;
     map<int,int>cnt;
     for(int i=2;i*i<=n;i++){
-        while(n%i==0){
-            cnt[i]++;
-            n/=i;
+        if(n%i==0){
+            while(n%i==0){
+                cnt[i]++;
+                n/=i;
+            }
         }
     }
     if(n>1)cnt[n]++;
