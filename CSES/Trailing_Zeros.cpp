c58916118c33eac80db7aaa364c5d87d69364e37
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-01-02 22:52:41 +0600

    mission accomplished 🔥🚀🔥

---
+++ CSES/Trailing_Zeros.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n,cnt=0;cin>>n;
+    ll x=1;
+    while(n>=x){
+        cnt+=(n/5);
+    }
+}
+int main(){
+    FAST;
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
