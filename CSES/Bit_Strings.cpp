47cb5ddde56550d04acc635987b27202e5debd92
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-01-02 22:29:02 +0600

    mission accomplished 🔥🚀🔥

--- CSES/Bit_Strings.cpp
+++ CSES/Bit_Strings.cpp
@@ -1,1 +1,30 @@
-cp
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+const ll mod=1000000007;
+void solve(){
+    ll n;cin>>n;
+    ll ans=1;
+    while(n--){
+        ans*=2;
+        ans%=mod;
+    }
+    cout<<ans;
+}
+int main(){
+    FAST;
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
