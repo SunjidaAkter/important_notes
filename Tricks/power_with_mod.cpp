216b90c569ac79510d9630c6a18833fdd8dc4fce
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-02-22 20:02:22 +0600

    mission accomplished 🔥🚀🔥

---
+++ Tricks/power_with_mod.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+const int MOD = 1e9 + 7;
+unsigned ll power(ll x, ll y, ll p =  MOD)
+{
+    unsigned ll res = 1;
+    x = x % p;
+    while (y > 0)
+    {
+        if (y & 1)
+            res = (res * x) % p;
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+void solve(){
+    ll n=power(2,5,LONG_MAX);
+    cout<<n<<nl;
+}
+int main(){
+    FAST;
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
